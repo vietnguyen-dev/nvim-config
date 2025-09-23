57eeb3b715ebd54f9b38b6356c4c317e76df7f77
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2025-09-22 22:01:31 -0700

    nvim complete

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,47 @@
 -- ~/.config/nvim/lua/plugins/conform.lua
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" }, -- load when saving
-  config = function()
-    -- If you install tools via Mason, make sure Conform can find them:
-    -- (Do this once somewhere early in your config, e.g. init.lua)
-    -- local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
-    -- if not string.find(vim.env.PATH or "", mason_bin, 1, true) then
-    --   vim.env.PATH = vim.env.PATH .. ":" .. mason_bin
-    -- end
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" }, -- load when saving
+	config = function()
+		-- If you install tools via Mason, make sure Conform can find them:
+		-- (Do this once somewhere early in your config, e.g. init.lua)
+		-- local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
+		-- if not string.find(vim.env.PATH or "", mason_bin, 1, true) then
+		--   vim.env.PATH = vim.env.PATH .. ":" .. mason_bin
+		-- end
 
-    require("conform").setup({
-      -- Map filetypes to formatters (order = priority)
-      formatters_by_ft = {
-        -- TS / TSX
-        typescript = { "prettier","prettierd" },
-        typescriptreact = { "prettier", "prettierd" }, -- TSX
-        javascript = { "prettier", "prettierd" },
-        javascriptreact = { "prettier", "prettierd"  },
+		require("conform").setup({
+			-- Map filetypes to formatters (order = priority)
+			formatters_by_ft = {
+				-- TS / TSX
+				typescript = { "prettier", "prettierd" },
+				typescriptreact = { "prettier", "prettierd" }, -- TSX
+				javascript = { "prettier", "prettierd" },
+				javascriptreact = { "prettier", "prettierd" },
 
-        -- Go
-        go = { "goimports", "gofumpt" }, -- goimports fixes imports; gofumpt enforces style
+				-- vanilla html css
+				html = { "prettier", "prettierd" },
 
-        -- SQL
-        sql = { "sqlfluff" }, -- or { "pg_format" } if you prefer
-      },
+				-- Go
+				go = { "goimports", "gofumpt" }, -- goimports fixes imports; gofumpt enforces style
 
-      -- Autoformat on save
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_fallback = true, -- use LSP if no formatter is found
-      },
-    })
+				-- SQL
+				sql = { "sqlfluff" }, -- or { "pg_format" } if you prefer
 
-    -- Optional manual keymap: <leader>f to format
-    vim.keymap.set({ "n", "v" }, "<leader>f", function()
-      require("conform").format({ async = true, lsp_fallback = true })
-    end, { desc = "Format file (Conform)" })
-  end,
+				--PHP
+				php = { "pretty-php" },
+			},
+
+			-- Autoformat on save
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true, -- use LSP if no formatter is found
+			},
+		})
+
+		-- Optional manual keymap: <leader>f to format
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, { desc = "Format file (Conform)" })
+	end,
 }
-
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,4 +12,3 @@ return {
     lspconfig.tailwindcss.setup({})
   end,
 }
-
